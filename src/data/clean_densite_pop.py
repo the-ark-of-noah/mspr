7b8296711a20d3b9d1da8c0e6dcb27609b6fc6ef
Erwan <erwan8989@outlook.fr>
2024-02-26 12:49:06 +0100

    Data quality densité

--- src/data/clean_densite_pop.py
+++ src/data/clean_densite_pop.py
@@ -8,7 +8,10 @@ df_densite = pd.read_excel(file_path, skiprows=4)
 # Renommer les colonnes pour la clarté
 df_densite = df_densite.rename(columns={'codgeo': 'code_postal', 'libgeo': 'departement'})
 
-print(df_densite.dtypes)
+# Convertir et ajuster les numéros pour la Corse
+df_densite["code_postal"] = df_densite["code_postal"].replace({'2A': "201", '2B': "202"})
+
+df_densite['code_postal'] = df_densite['code_postal'].astype(int)
 
 output_path = "../../data/processed/densite_population_depuis_1968.csv"
 df_densite.to_csv(output_path, index=False)
