84c6c7c61a608905224a5728e3990024ddaf6adb
Erwan <erwan8989@outlook.fr>
2024-02-26 11:08:53 +0100

    Tranche d'age nettoyé

--- src/data/clean_tranche_age.py
+++ src/data/clean_tranche_age.py
@@ -1,54 +1,31 @@
-import sys
 import pandas as pd
 
-def main():
-    clean_data()
+# Charger le fichier CSV dans un DataFrame
+df = pd.read_csv('../../data/raw/2020_2023_tranche_age.csv', low_memory=False, delimiter=';')
 
+# Renommer les colonnes
+column_to_rename = {
+    "Libellé": "departement",
+    "Code": "code_departement",
+    "Part des pers. âgées de 65 ans ou + 2020": "65-99_2020",
+    "Estimations de population : part des 60 ans ou plus 2023": "60-99_2023",
+    "Estimations de population : part des 25 à 59 ans 2023": "25-59_2023",
+    "Estimations de population : part des 0 à 24 ans 2023": "0-24_2023",
+    "Part des pers. âgées de - de 25 ans 2020": "0-25_2020",
+    "Part des pers. âgées de 25 à 64 ans 2020": "25-64_2020"
+}
 
-def clean_data():
-    folder_path = "../../data/raw/"
-    file = open(f"{folder_path}tranche_dage_2020-2023.csv", "rb")
-    data = pd.read_csv(file, sep=";", header=2, encoding="utf-8")
-    labels = data["Libellé"]
-    n = 100
+# Renommer les colonnes
+df = df.rename(columns=column_to_rename)
 
-    # print(data.columns)
+# Convertir et ajuster les numéros pour la Corse
+df["code_departement"] = df["code_departement"].replace({'2A': "201", '2B': "202"})
 
-    # clean 2020 data
-    data = data[data["Part des pers. âgées de - de 25 ans 2020"].apply(lambda x: is_float(x))]
-    data = data[data["Part des pers. âgées de 25 à 64 ans 2020"].apply(lambda x: is_float(x))]
-    data = data[data["Part des pers. âgées de 65 ans ou + 2020"].apply(lambda x: is_float(x))]
+# Supprimer la ligne ayant le code département 971
+df = df[df['code_departement'] != 971]
 
-    # clean 2023 data
-    pd.to_numeric(data["Estimations de population : part des 0 à 24 ans 2023"], errors="coerce")
-    pd.to_numeric(data["Estimations de population : part des 25 à 59 ans 2023"], errors="coerce")
-    pd.to_numeric(data["Estimations de population : part des 60 ans ou plus 2023"], errors="coerce")
+# Sauvegarde
+output_path = "../../data/processed/tranche_age_cleaned.csv"
+df.to_csv(output_path, index=False)
 
-    # encode label column correctly using utf-8
-    data["Libellé"] = labels.str.encode("utf-8").str.decode("utf-8")
-
-
-    # merge columns by age range
-    # # data_less255 = data[["Part des pers. âgées de - de 25 ans 2020", "Estimations de population : part des 0 à 24 ans 2023"]]
-    # data_less255 = data["Part des pers. âgées de - de 25 ans 2020"] + data["Estimations de population : part des 0 à 24 ans 2023"]
-
-
-
-    print(data.columns)
-    write_to_csv(data, "tranche_dage_2023")
-
-
-def write_to_csv(data: pd.DataFrame, file_name: str):
-    folder_path = "../../data/processed/"
-    data.to_csv(f"{folder_path}{file_name}.csv", index=False, sep=";", encoding="utf-8")
-
-def is_float(x):
-    try:
-        float(x)
-    except ValueError:
-        return False
-    return True
-
-
-if __name__ == '__main__':
-    sys.exit(main())
+print("Tranche d'age sauvegardé avec succès")
